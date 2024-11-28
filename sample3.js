d783e5b8e26dd5773fe093fe3a117a3b7f7bd660
sindhuviswanadh <sindhuviswanadh17@gmail.com>
2024-11-28 18:38:33 +0530

    Add files via upload

---
+++ sample3.js
@@ -0,0 +1,5 @@
+// Function to handle connecting to a device
+function connectToDevice(deviceName) {
+    alert(`Connecting to ${deviceName}...`);
+    // Add real connection logic here if needed.
+}
